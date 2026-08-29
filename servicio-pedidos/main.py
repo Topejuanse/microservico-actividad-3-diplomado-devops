6be28b3f951879ffff55426ff7fcf9abdf4d6d03
Jeisson Fuquene <fuquene.corp@gmail.com>
2026-08-28 22:35:33 -0500

    Add 'Articulo #5' to pedidos response

--- servicio-pedidos/main.py
+++ servicio-pedidos/main.py
@@ -6,4 +6,4 @@ app = FastAPI()
 
 def get_pedidos():
 
-    return {"servicio": "pedidos", "status": "ok", "data": ["Articulo #1", "Articulo #2", "Articulo #3","Articulo #4"]}
+    return {"servicio": "pedidos", "status": "ok", "data": ["Articulo #1", "Articulo #2", "Articulo #3","Articulo #4", "Articulo #5"]}
